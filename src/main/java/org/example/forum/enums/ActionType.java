832b20d6ca9dbe03c0b49e82394321b24d3b9ff5
Artur Leszczak <al.ok1@wp.pl>
2024-06-20 03:08:50 +0200

    Dokumentacja enuma

--- src/main/java/org/example/forum/enums/ActionType.java
+++ src/main/java/org/example/forum/enums/ActionType.java
@@ -1,5 +1,15 @@
 package org.example.forum.enums;
 
+/**
+ * Enum przechowujące wszystkie możliwe stany reprezentujące akcje wykonane na
+ * obiektach zarządzalnych przez zewnętrznych użytkowników aplikacji.
+ *
+ * Używane w => @see org.example.forum.repositories.ActionRepository oraz org.example.forum.services.ActionService
+ *
+ * @see db.migration V1.0.8__Create_action_types_tables.sql
+ * @author Artur Leszczak
+ * @version 1.0.0
+ */
 public enum ActionType {
     ADD(1),
     EDIT(2),
@@ -12,12 +22,12 @@ public enum ActionType {
 
     private final int value;
 
-    // Konstruktor
+    // Konstruktor enuma
     ActionType(int value) {
         this.value = value;
     }
 
-    // Getter do pobierania wartości
+    // Getter do pobierania wartości enuma
     public int getValue() {
         return value;
     }
